50008dffc308e771689fddba63b660a388378acb
Martin Stolz <martin@ustwo.com>
2015-01-19 12:55:53 +0000

    Added missing iOS element ids

--- features/support/element_ids_ios.rb
+++ features/support/element_ids_ios.rb
@@ -5,6 +5,8 @@ class ElementIdsIos
     map = Hash.new
     map[:commitlist_button_refresh] = 'commitlist_button_refresh'
     map[:commitlist_list_row_label_author] = 'commitlist_list_row_label_author'
+    map[:commitlist_no_commits_indicator] = 'commitlist_no_commits_indicator'
+    map[:commitlist_title] = 'commitlist_title'
 
     return map
   end
