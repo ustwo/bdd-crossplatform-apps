9b5df42fb4ea1c125e2ab017ca4e8a3f4d070329
Martin Stolz <martin@ustwo.com>
2015-01-23 17:57:47 +0000

    Formatted code

--- features/step_definitions/screens/ios_commit_list_screen.rb
+++ features/step_definitions/screens/ios_commit_list_screen.rb
@@ -3,8 +3,9 @@ require_relative 'commit_list_screen'
 class IosCommitListScreen < CommitListScreen
 	
 	def ids
-
 	    map = Hash.new
+
+	    # Commit List Screen
 	    map[:commitlist_button_refresh] = 'commitlist_button_refresh'
 	    map[:commitlist_list_row_label_author] = 'commitlist_list_row_label_author'
 	    map[:commitlist_no_commits_indicator] = 'commitlist_no_commits_indicator'
