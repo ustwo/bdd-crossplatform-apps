45c6e39eda58aa8773a70b50fc36b6f8f48c3322
Martin Stolz <martin@ustwo.com>
2015-01-28 11:16:47 +0000

    Sorted commit list screen's methods

--- features/step_definitions/screens/commit_list_screen.rb
+++ features/step_definitions/screens/commit_list_screen.rb
@@ -2,39 +2,45 @@ require_relative 'base_screen'
 
 class CommitListScreen < BaseScreen
 
-	def get_title
-		get_text(:commitlist_title)
-	end
-
-	def has_commit_message text
-		!$driver.find_element(name: text).nil?
-	end
-
-	def has_date text
-		!$driver.find_element(name: text).nil?
-	end
-
-	def has_no_commits_indicator
-		$driver.find_element(id: ids[:commitlist_no_commits_indicator]).displayed?
-	end
-
-	def has_commits_error_indicator
-		$driver.find_element(id: ids[:commitlist_no_commits_indicator]).displayed?
-	end
-
-	def shows_commits_error
-		$driver.find_element(id: ids[:commitlist_no_commits_indicator]).displayed?
-	end
-
-	def get_number_of_commits
-		$driver.find_elements(name: @screen.ids[:commit_list_list_row]).count
-	end
-
+	# Generic
 	def get_text id
 		$driver.find_element(id: ids[id]).text
 	end
 
+	# Title
+	def get_title
+		get_text(:commitlist_title)
+	end
+
+	# Commit list
 	def get_commit_list
 		$driver.find_element(id: ids[:commitlist_list])
+	end
+
+	# Commit message
+	def has_commit_message text
+		!$driver.find_element(name: text).nil?
+	end
+
+	# Commit date
+	def has_date text
+		!$driver.find_element(name: text).nil?
+	end
+
+	# Error indicators
+	def get_commits_error_indicator
+		$driver.find_element(id: ids[:commitlist_no_commits_indicator])
+	end
+
+	def has_commits_error_indicator
+		get_commits_error_indicator.displayed?
+	end
+
+	def get_commits_error
+		get_commits_error_indicator.text
+	end
+
+	def get_number_of_commits
+		$driver.find_elements(name: @screen.ids[:commit_list_list_row]).count
 	end
 end
