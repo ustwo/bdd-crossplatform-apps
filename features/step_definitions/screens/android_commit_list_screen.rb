9ffece65d59ec7cdcb85f916685ff3ae3df35483
Martin Stolz <martin@ustwo.com>
2015-01-29 17:24:57 +0000

    Using new element id

--- features/step_definitions/screens/android_commit_list_screen.rb
+++ features/step_definitions/screens/android_commit_list_screen.rb
@@ -28,7 +28,7 @@ class AndroidCommitListScreen < CommitListScreen
 	    map[:commitlist_loading_indicator] = 'android:id/progress'
 
 	    # Commit List Row
-	    map[:commitlist_row] = 'com.ustwo.sample:id/commit_list_linearlayout_row'
+	    map[:commit_list_list_row] = 'com.ustwo.sample:id/commit_list_linearlayout_row'
 	    map[:commitlist_row_textview_date] = 'com.ustwo.sample:id/commit_list_row_textview_date'
 	    map[:commitlist_row_textview_message] = 'com.ustwo.sample:id/commit_list_row_textview_message'
 	    map[:commitlist_list] = 'com.ustwo.sample:id/commit_list_listview_commits'
