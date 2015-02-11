815a3b5b6b4d526ddb1ec914ae313c3d5631250f
Juan Delgado <juan@ustwo.com>
2015-02-11 14:55:31 +0000

    Now hardcoding scheme and build dir inside the XCode build command.
    This is the *opposite* of what we thought we would as per #11 and #13. The truth is that this is not a tutorial about how to compile mobile apps from the terminal.
    
    This it not a project template that you can clone, change a few configuration values and get the project up and running quickly.
    
    This project is a reference or guide about our BDD set up.
    
    Nothing more, nothing less!

--- features/support/commands/xcodebuild_command.rb
+++ features/support/commands/xcodebuild_command.rb
@@ -2,7 +2,9 @@ require_relative 'blocking_command'
 
 class XCodeBuildCommand < BlockingCommand
 
-  def initialize scheme, configuration, build_dir
-    @cmd = "xcodebuild -scheme #{scheme} -derivedDataPath #{build_dir} -configuration #{configuration} OBJROOT=#{build_dir} SYMROOT=#{build_dir} -sdk iphonesimulator build"
+  def initialize
+    build_dir = 'build/'
+    scheme = 'AppTestingSample-BDD'
+    @cmd = "xcodebuild -scheme #{scheme} -derivedDataPath #{build_dir} -configuration #{scheme} OBJROOT=#{build_dir} SYMROOT=#{build_dir} -sdk iphonesimulator build"
   end
 end
