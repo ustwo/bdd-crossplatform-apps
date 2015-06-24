cc44a4bfd8bf92048b61ed0ef10abc6cca170c6c
Juan Delgado <juan@ustwo.com>
2015-06-24 22:58:09 +0100

    Fixed Lint/UnusedMethodArgument for a couple of abstract methods.

--- features/step_definitions/screens/commit_list_screen.rb
+++ features/step_definitions/screens/commit_list_screen.rb
@@ -1,8 +1,8 @@
- # 
+ #
  # The MIT License (MIT)
- # 
+ #
  # Copyright (c) 2015 ustwo™
- # 
+ #
  # Permission is hereby granted, free of charge, to any person obtaining a copy
  # of this software and associated documentation files (the "Software"), to deal
  # in the Software without restriction, including without limitation the rights
@@ -12,7 +12,7 @@
 
  # The above copyright notice and this permission notice shall be included in all
  # copies or substantial portions of the Software.
- # 
+ #
  # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,6 +44,7 @@ class CommitListScreen < BaseScreen
 	end
 
 	# Commit list
+	# rubocop:disable Lint/UnusedMethodArgument
 	def get_commit index
 		raise 'Abstract'
 	end
